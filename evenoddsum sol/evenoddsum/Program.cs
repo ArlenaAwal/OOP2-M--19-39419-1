d7145f7955a6ede4cbc5cd68fe49d47e5b8e367d
Arlena Awal <arlenawalchaity@gmail.com>
2021-02-08 09:51:15 +0600

    even number

---
+++ evenoddsum sol/evenoddsum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace evenoddsum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            int n = 100;
+            int sum1=0;
+            int sum2=0;
+            for(i=0;i<=n;i++)
+            {
+                if(i%==0)
+                {
+                }
+            }
+        }
+    }
+}
