74fb06ca45c1489408cc49a3149bc696a810c0b8
Arlena Awal <arlenawalchaity@gmail.com>
2021-02-08 10:57:02 +0600

    lab3

---
+++ lab3 sol/lab3/Student.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Student
+    {
+        private String name, id, department;
+        private float cgpa;
+
+        public Student(String name, String id, String department, float cgpa)
+        {
+            name= this.name;
+            id= this.id;
+            department= this.department;
+            cgpa= this.cgpa;
+        }
+
+    }
+}
---
+++ lab3 sol/lab3/Triangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    class Triangle
+    {
+        private int x = 0;
+         private int y = 0;
+         private int z = 0;
+
+    }
+}
